a7a4acbf4f2d46dc66ca0c3abc53d9e69b2be0a1
Frano <fxn3380@g.rit.edu>
2017-04-14 22:17:20 +0200

    Added Interface for object classes

---
+++ Project/assets/php/InterfaceClass.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Frano
+ * Date: 14. 4. 2017.
+ * Time: 10:14 PM
+ */
+interface InterfaceClass
+{
+
+    public function fetch();
+    public function put();
+    public function post();
+    public function delete();
+
+}
