9e417c9cf8ea4301f97e52a80f2881213e2b3f14
Frano <fxn3380@g.rit.edu>
2017-04-14 11:48:36 +0200

    Added project structure

---
+++ Project/assets/includes/footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Frano
+ * Date: 14. 4. 2017.
+ * Time: 11:21 AM
+ */
+
+?>
+
+    </body>
+    <script src="<?php echo $path; ?>assets/js/script.js\"></script>
+</html>
---
+++ Project/index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Frano
+ * Date: 14. 4. 2017.
+ * Time: 11:14 AM
+ */
+
+$page = 'Homepage';
+$path = './';
+
+include($path . 'assets/includes/header.php');
+
+?>
+
+<!--HTML code-->
+
+<?php
+
+include($path . 'assets/includes/footer.php');
+
+?>
