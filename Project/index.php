cb8be3b764f348fd0507c8eaf1c96f79b8273fe2
Frano <fxn3380@g.rit.edu>
2017-04-19 15:23:50 +0200

    Updated some things

--- Project/index.php
+++ Project/index.php
@@ -13,9 +13,24 @@ include($path . 'assets/includes/header.php');
 
 ?>
 
-<!--HTML code-->
+<div id="box">
 
-<!-- Event card -->
+    <div id="text">
+        <h1>Title</h1>
+        <h5>Random text</h5>
+        <p>Lorem ipsum dolor sit amet, enim eiusmod commodo vel metus, vitae nec, tellus eros porta consectetuer.
+            Nascetur
+            et rutrum nunc nascetur dictumst est, consequat nulla. Cursus et adipiscing eu, adipiscing proin a ut
+            nunc. Wisi
+            risus, eget sed neque libero. Volutpat lorem sed turpis vestibulum, venenatis eu et luctus auctor nulla,
+            sollicitudin orci.</p>
+    </div>
+    <div id="login" class="mdl-card mdl-shadow--6dp">
+        <form>
+
+        </form>
+    </div>
+</div>
 
 <?php
 
