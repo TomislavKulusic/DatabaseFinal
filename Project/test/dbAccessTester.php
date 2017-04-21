1f0c9362aaea072696f85505f6b99f74f9d98a09
Frano <fxn3380@g.rit.edu>
2017-04-21 21:56:11 +0200

    Updated some things

--- Project/test/dbAccessTester.php
+++ Project/test/dbAccessTester.php
@@ -6,38 +6,41 @@
  * Time: 04:53 PM
  */
 
-include_once("../assets/php/TheDatabase.php");
-include_once("../assets/php/Renter.php");
-include_once("../assets/php/User.php");
+include('../application/configs/config.php');
+
+include_once(LIBRARY_PATH . "TheDatabase.php");
+include_once(LIBRARY_PATH . "Renter.php");
+include_once(LIBRARY_PATH . "User.php");
 
 $page = "Test";
 $path = "../";
 
-include("../assets/includes/header.php");
+include(TEMPLATES_PATH . "header.php");
 
-$database = new TheDatabase("localhost", "root", "root", "mydb");
+$database = new TheDatabase($config['db']['host'], $config['db']['username'], $config['db']['password'], $config['db']['dbName']);
 
 if ($database->connect()) {
     echo "Connected<br><br>";
 
-/*    $renter = new Renter('1', '', '', '', '', $database);
+    /*    $renter = new Renter('1', '', '', '', '', $database);
 
-    $renter->fetch();
+        $renter->fetch();
 
-    $renter->setRentedMovies(true);
+        $renter->setRentedMovies(true);
 
-    print_r($renter->getRentedMovies());*/
+        print_r($renter->getRentedMovies());
 
-    $user = new User("Pero", "Peric", "", $database);
+        $user = new User("Pero", "Peric", "", $database);
 
-    if ($user->login())
-        echo "Ok";
-    else
-        echo "Error";
+        if ($user->login())
+            echo "Ok";
+        else
+            echo "Error";
+    */
 
     if ($database->close()) {
         echo "<br><br>Closed";
     }
 }
 
-include('../assets/includes/footer.php');
+include(TEMPLATES_PATH . 'footer.php');
