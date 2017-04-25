949583fa64b7500b832bba9329fa6d414065852f
Frano <fxn3380@g.rit.edu>
2017-04-25 13:40:09 +0200

    Few stuff

---
+++ Project/application/pages/cart.php
@@ -0,0 +1,23 @@
+<?php
+
+include(TEMPLATES_PATH . "navigation.php");
+
+?>
+<main class="mdl-layout__content">
+    <div class="flex">
+        <div id="items" class="flex column">
+            <div class="item">
+                <div class="img1"></div>
+            </div>
+
+            <div class="item">
+                <div class="img2"></div>
+            </div>
+        </div>
+
+        <div id="checkout">
+
+        </div>
+    </div>
+</main>
+</div>
