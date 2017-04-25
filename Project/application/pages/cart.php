1ba6745ddfe99d3f7bbe89b378ae56f8e6d3980d
Frano <fxn3380@g.rit.edu>
2017-04-25 15:26:02 +0200

    Edited Cart

--- Project/application/pages/cart.php
+++ Project/application/pages/cart.php
@@ -4,14 +4,19 @@ include(TEMPLATES_PATH . "navigation.php");
 
 ?>
 <main class="mdl-layout__content">
+    <h3>Cart</h3>
+
     <div class="flex">
         <div id="items" class="flex column">
-            <div class="item">
-                <div class="img img1"></div>
+            <div class="item flex">
+                <div class="img img1">
+                </div>
+                <div class="title">Wings</div>
             </div>
 
-            <div class="item">
+            <div class="item flex">
                 <div class="img img2"></div>
+                <div class="title">Neki Film</div>
             </div>
         </div>
 
