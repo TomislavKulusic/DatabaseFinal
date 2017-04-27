cdf00661c96e000bb20750a8478d8fc67caf1931
TomislavKulusic <txk8421@g.rit.edu>
2017-04-27 23:20:03 +0200

    Add files via upload
    Feat: Added post for storing data to watch table

---
+++ Project/public/js/watchLater.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Tomislav on 4/27/2017.
+ */
+
+$(document).ready(function() {
+
+
+    $("#test").click(function () {
+
+
+        $.post('watchLaterMovie.php', function (resp) {
+
+
+        });
+
+
+
+
+    });
+       
+       
+    
+        
+
+    
+
+
+});
+
+
+
